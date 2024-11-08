e1136aa2ec4d661c2bf2bfa2aca69e5060aad092
ajit267 <ajitkumarkrwk@gmail.com>
2024-11-08 10:55:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('🤖 Machine Learning App')
 
 st.info("This app builds a machine learning model")
 
-df = pd.read_csv("https://raw.githubusercontent.com/ajit267/Diabetes_Prediction/refs/heads/main/diabetes.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/shsarv/Machine-Learning-Projects/refs/heads/main/Diabetes%20Prediction%20%5BEND%202%20END%5D/dataset/kaggle_diabetes.csv")
 df.head()
