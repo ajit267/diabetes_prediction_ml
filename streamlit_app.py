d07ce2730c58401dac9799d377fbc556b5bb4c74
ajit267 <ajitkumarkrwk@gmail.com>
2024-11-08 11:31:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,5 +23,13 @@ with st.expander('Data Visualization'):
 # Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age
 with st.sidebar:
   st.header('Input Features')
-  Pregnancies = st.slider('No OF Pregnancies',0,10,5)
+  Pregnancies = st.slider('No OF Pregnancies',0,18,3)
+  Glucose = st.slider('Glucose Level',0,200,121)
+  BloodPressure = st.slider('BloodPressure:BP',0,122,69)
+  SkinThickness = st.slider('SkinThickness',0,110,21)
+  Insulin = st.slider('Insulin',0,744,80)
+  BMI = st.slider('BMI',0,80,32)
+  DiabetesPedigreeFunction = st.slider('DiabetesPedigreeFunction',0.00,2.45,0.47)
+  Age = st.slider('Age',21,81,33)
+  
 
