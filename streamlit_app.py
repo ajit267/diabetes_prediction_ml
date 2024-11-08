bc33ce1e81731fea775d2ea2256e7df6864f6d5d
ajit267 <ajitkumarkrwk@gmail.com>
2024-11-08 11:10:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,4 +17,4 @@ with st.expander('Data'):
   y
 
 with st.expander('Data Visualization'):
-  st.scatter_chart(data=df,x='BloodPressure',y='Age',color='Outcome')
+  st.scatter_chart(data=df,x='Age',y='BloodPressure',color='Outcome')
